013334bbc0b3f44cd7b1bc0909182d843190f734
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-10-17 16:44:32 -0600

    correcciones

--- proyecto final.py
+++ proyecto final.py
@@ -1,14 +1,1 @@
-class Porducto:
-    pass
-class Usuario:
-    pass
-class Listado:
-    pass
-class Empleado:
-    pass
-class Venta:
-    pass
-class Compra:
-    pass
-class Proveedor:
-    pass
+
