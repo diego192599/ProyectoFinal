c13e7accf90c4ba53ade1ef1438e3ed76c4ecc38
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-10-17 16:43:24 -0600

    creacion de clases 2

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Interfaz grafica")
+class Porducto:
+    pass
+class Usuario:
+    pass
+class Listado:
+    pass
+class Empleado:
+    pass
+class Venta:
+    pass
+class Compra:
+    pass
+class Proveedor:
+    pass
