7dbe293668568f034a14cce37f239c7a8dd75f1d
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-10-17 16:09:24 -0600

    creacion de clases

---
+++ proyecto final.py
@@ -0,0 +1,14 @@
+class Porducto:
+    pass
+class Usuario:
+    pass
+class Listado:
+    pass
+class Empleado:
+    pass
+class Venta:
+    pass
+class Compra:
+    pass
+class Proveedor:
+    pass
